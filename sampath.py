35ab1f97b1dfd231382ca3064ca0644fa98d8fdd
sampath nayak <bsampath026@gmail.com>
2026-07-01 16:38:18 +0530

    python simple program

---
+++ sampath.py
@@ -0,0 +1,28 @@
+name = "John"          # String
+age = 24               # Integer
+salary = 25000.75      # Float
+status = True          # Boolean
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(status))
+
+colors = ["Red", "Blue", "Green"]
+
+print(colors)
+
+print(colors[0])
+
+colors.append("Yellow")
+
+colors.remove("Blue")
+
+print(colors)
+
+
+fruits = ("Apple", "Banana", "Orange")
+
+print(fruits)
+
+print(fruits[1])
